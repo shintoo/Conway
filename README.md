e74b32c5d780f6e4a9eb82cecc68842c88979c09
shintoo <seanprapp@gmail.com>
2015-05-18 14:07:09 -0400

    Speed parameter and psuedo-infinite generations

--- README.md
+++ README.md
@@ -5,8 +5,8 @@ Implementation of the popular hit, "Conway's Game of Life"!
 
 ##Usage
 ```
-./Life <generations> <text file>
+./Life <generations> <speed 1-10> <text file>
 ``` 
-If generations is 0, it will run until killed externally.  
+If generations is 0, it will run until killed externally.
  
 As of right now, you must surround your grid with a border for it to work correctly. See the example to get the idea (no alive cells on border).
--- main.c
+++ main.c
@@ -1,21 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <time.h>
+#include <string.h>
+#include <limits.h>
+
 #include "life.h"
 
 int main(int argc, char **argv ) {
+	if (argc < 4) {
+		printf("usage: %s <generations> <speed> <text file>\n",
+		       argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	
 	int generations = atoi(argv[1]);
-	struct timespec req = {0, 100 * 1000 * 1000};
+	
+	int speed = atoi(argv[2]);
+	if (speed > 10) {
+		speed = 10;
+	}
+	if (speed < 1) {
+		speed = 1;
+	}
+	speed = 11 - speed;
+
+	struct timespec req = {0, speed * 50 * 1000 * 1000};
 	struct timespec rem = {0,0};
-	FILE *in = fopen(argv[2], "r");
+	bool random;
+	int randx, randy;
+	FILE *in;
+
+	if (strncmp(argv[3], "random", 6) == 0) {
+
+		random = true;
+	}
+	else if ((in = fopen(argv[3], "r")) == NULL) {
+		fprintf(stderr, "%s not found.\n", argv[3]);
+		exit(EXIT_FAILURE);
+	}
 
 	Field *Conway = NewField();
+	switch (random) {
+//		case true: Conway->Random(Conway); break;
+		case false: Conway->Read(Conway, in); break;
+	}
 
-	Conway->Read(Conway, in);
-	
 	puts("Generation:\t1");
 	Conway->Print(Conway);
-	for (int i = 2; i <= generations || generations == 0; i++) {
+	for (int i = 2; i <= generations || (generations == 0 && i < INT_MAX); i++) {
 		Conway->Evolve(Conway);
 		printf("\n\nGeneration:\t%d\n", i);
 		Conway->Print(Conway);
